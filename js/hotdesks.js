e2f786c9337b5dbfaae3ee070b8b95ab5a294b34
kowalcj0 <kowalczykjanusz@gmail.com>
2014-11-28 11:14:12 +0000

    added emp desks

--- js/hotdesks.js
+++ js/hotdesks.js
@@ -25,11 +25,50 @@ var hotdesks = [
 ]
 
 
+var empdesks = [
+["ED01","Alex Vail","Events Manager","alex.vail@cde.catapult.org.uk",""],
+["ED02","Allyssa Gregory","HR Manager","allyssa.gregory@cde.catapult.org.uk",""],
+["ED03","Avril Grieves","","avril.grieves@cde.catapult.org.uk",""],
+["ED04","Azita Esmaili","Chief Development Officer","azita@cde.catapult.org.uk",""],
+["ED05","Carly Glowotz","HR Specialist","carly.glowotz@cde.catapult.org.uk","07964572424"],
+["ED06","Casey Richardson","","casey.richardson@cde.catapult.org.uk",""],
+["ED07","Chirdeep Chhabra","","chirdeep.chhabra@cde.catapult.org.uk","+44 7972770116"],
+["ED08","Chris Thompson","Partnerships Director","chris.thompson@cde.catapult.org.uk","07904832928"],
+["ED09","Claire Mackay","Office Manager","claire.mackay@cde.catapult.org.uk","0775 1191 807"],
+["ED10","Clare Brooke","","clare.brooke@cde.catapult.org.uk",""],
+["ED11","Daniel Casadei","","Dan.Casadei@cde.catapult.org.uk",""],
+["ED12","Daniel Dearing","Partnerships Manager","daniel.dearing@cde.catapult.org.uk","07970227860"],
+["ED13","David Sparks","Operations Manager","david.sparks@cde.catapult.org.uk",""],
+["ED14","Dennis Temper","","dennis.temper@cde.catapult.org.uk",""],
+["ED15","Finance Finance","","finance@cde.catapult.org.uk",""],
+["ED16","Gerrod Taylor","","gerrod.taylor@cde.catapult.org.uk",""],
+["ED17","hamid falaki","Connected Digital Economy Catapult","hamid.falaki@cde.catapult.org.uk",""],
+["ED18","Irene Lopez de Vallejo","","Irene.lopezdevallejo@cde.catapult.org.uk",""],
+["ED19","jenni young","","jenni.young@cde.catapult.org.uk",""],
+["ED20","Lee Tarrant","Head of Test and Engineering","","lee.tarrant@cde.catapult.org.uk","0203 574 4894"],
+["ED21","Maame-Yaa Bempah","Financial Controller","mbempah@cde.catapult.org.uk","07964 140 565"],
+["ED22","Marko Balabanovic","Innovation Director","marko.balabanovic@cde.catapult.org.uk",""],
+["ED23","Matt Leipnik","Information Systems Manager","matt.leipnik@cde.catapult.org.uk","07949935777"],
+["ED24","Matt Ward","","matt.ward@cde.catapult.org.uk",""],
+["ED25","Maurizio Pilu","","maurizio.pilu@cde.catpult.org.uk",""],
+["ED26","Michella Oswald","Events Manager","michella.oswald@cde.catapult.org.uk","077712545115"],
+["ED27","Miles Davenport","","miles.davenport@cde.catapult.org.uk",""],
+["ED28","Neil Crockett","","neil.crockett@cde.catapult.org.uk",""],
+["ED29","Nithin Raj","Programme Manager","nithin.raj@cde.catapult.org.uk",""],
+["ED30","Noeline Sanders","Chief Business Officer","noeline.sanders@cde.catapult.org.uk","07786 916826"],
+["ED31","Pat McAlinden","EA to CEO","pat.mcalinden@cde.catapult.org.uk","07964324029"],
+["ED32","Paul Galwas","","paul.galwas@cde.catapult.org.uk","442035744832"],
+["ED33","Richard French","","richard.french@cde.catapult.org.uk",""],
+["ED34","Sade Laja","Head of digital engagement and content","sade.laja@cde.catapult.org.uk","+44 (0) 203 574 4707"],
+["ED35","Steven Wood","Senior Partnerships Manager","steven.wood@cde.catapult.org.uk","07972871092"],
+["ED36","Temitope Adejumo","Accounts Assistant","temitope.adejumo@cde.catapult.org.uk","07946889261"]
+]
+
+
 String.format = function() {
     // The string containing the format items (e.g. "{0}")
     // will and always has to be the first argument.
     var theString = arguments[0];
-    
     // start with the second argument (i = 1)
     for (var i = 1; i < arguments.length; i++) {
         // "gm" = RegEx options for Global search (more than one instance)
@@ -37,7 +76,6 @@ String.format = function() {
         var regEx = new RegExp("\\{" + (i - 1) + "\\}", "gm");
         theString = theString.replace(regEx, arguments[i]);
     }
-    
     return theString;
 }
 
@@ -47,29 +85,22 @@ String.capitalize = function() {
 }
 
 
-var fillDeskData = function() {
-    for (desk in hotdesks) {
-        $("#HD"+hotdesks[desk][0]).html(hotdesks[desk][2]);
-    }
-};
-
-
-var getDesk = function(deskId) {
+var getDesk = function(desks, deskId) {
     var desk = ["N/A","N/A","N/A","N/A","N/A","N/A","N/A"];
-    for (d in hotdesks) {
-        if (hotdesks[d][0] == deskId) {
-            console.log("found desk: "+deskId+" - "+hotdesks[d][4]);
-            desk = hotdesks[d];
+    for (d in desks) {
+        if (desks[d][0] == deskId) {
+            desk = desks[d];
         };
     };
     return desk;
 }
 
 
-var getDeskDetails = function(deskId) {
-   var desk = getDesk(deskId); 
+var getDeskDetails = function(desks, deskId) {
+   var desk = getDesk(desks, deskId); 
 
-   var cols = ["Desk","Name","Organistation","Email","Phone","Time in","Time Out"];
+   var HDcols = ["Desk","Name","Organistation","Email","Phone","Time in","Time Out"];
+   var EDcols = ["Desk","Name","Role","Email","Phone"];
    var output = "<table id='deskDetails' class='deskDetails'>";
 
    var cellTemplate = "<tr id='{0}Cell' class='tooltipCell'>"+
@@ -77,6 +108,9 @@ var getDeskDetails = function(deskId) {
                            "<td id='{3}Value' class='tooltipValue'>{4}</td>"+
                        "</tr>";
 
+   var cols = null;
+   if (desks[0].length == HDcols.length) { cols = HDcols; console.log("HDcols"); }
+   if (desks[0].length == EDcols.length) { cols = EDcols; console.log("EDcols"); }
    for (col in cols) {
         c = String.format(cellTemplate,
                           cols[col].replace(" ",""), 
@@ -86,31 +120,44 @@ var getDeskDetails = function(deskId) {
                           desk[col])
         output += c;
    }
-
    output += "</table>";
-   console.log(output);
-
    return output;
 }
 
 
 !function ($) {
   $(function(){
-    fillDeskData();
 
     $("div[id*='HD']").tooltip({
         items: "[id]",
         content: function() {
             var element = $( this );
-            return getDeskDetails(element.context.id);
+            return getDeskDetails(hotdesks, element.context.id);
         },
         show: "slideDown", // show immediately
         open: function(event, ui)
         {
             ui.tooltip.hover(
-            function () {
-                $(this).fadeTo("slow", 0.5);
-            });
+                    function () {
+                        $(this).fadeTo("slow", 0.5);
+                    });
+        }
+    });
+
+    $("div[id*='ED']").tooltip({
+        items: "[id]",
+        content: function() {
+            var element = $( this );
+            return getDeskDetails(empdesks, element.context.id);
+        },
+        //show: "slideDown", // show immediately
+        show: { effect: function() { $(this).fadeTo(300, 0.9);}, length:0},
+        open: function(event, ui)
+        {
+            ui.tooltip.hover(
+                    function () {
+                        $(this).fadeTo(300, 0.9);
+                    });
         }
     });
   });
