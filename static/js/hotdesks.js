d587818a9c56f1608456cbfc4806ced6c6fca7dd
cliff xuan <cliff.xuan@gmail.com>
2014-11-28 13:02:26 +0000

    added frontend stuff

---
+++ static/js/hotdesks.js
@@ -0,0 +1,205 @@
+var hotdesks = [
+["HDA1","Keith Reed","1234","keith.reed@1248.io","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDA2","Steve Lake","4 Corners","steve@4cornersimages.com","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDA3","Luke Hood","AEI Media"," luke.hood@aeimedia.co.uk","","28/11/2014 11:00","28/11/2014 13:00"],
+["HDA4","David Grundy","ALCS","David.Grundy@alcs.co.uk","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDB1","Keith Barfoot","AlphaFox Systems Ltd","crystal@alphafoxsystems.com","(0)1404 42455","28/11/2014 09:00","28/11/2014 17:00"],
+["HDB2","Thibaut Rouffineau","Apptual","thibaut.rouffineau@apptual.com","07768 421 323","28/11/2014 09:00","28/11/2014 17:00"],
+["HDB3","Jonathan Duffy","Beacontent","jonathan@beacontent.co.uk","0208 144 1961 / 0771 774 0257","28/11/2014 09:00","28/11/2014 17:00"],
+["HDB4","Stephen McConnachie","BFI","Stephen.McConnachie@bfi.org.uk","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDC1","Jamie Paton","Canary Care","jamie@canarycare.co.uk","07773 358 727","28/11/2014 09:00","28/11/2014 17:00"],
+["HDC2","Abbie Enock","Capture","abbie.enock@capture.co.uk ","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDC3","Patrick Bergel","Chirp","patrick@chirp.io","(0)7971 987 064","28/11/2014 09:00","28/11/2014 17:00"],
+["HDC4","Ali Hossaini","Cinema Arts Network","ali@cinema-arts.net","0783 454 2479","28/11/2014 09:00","28/11/2014 17:00"],
+["HDD1","James Bennett","CLA","james.bennett@cla.co.uk","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDD2","Shaun Cutler","Clixta","shaun@clixta.com","","28/11/2014 08:00","28/11/2014 12:00"],
+["HDD3","Sam Obernick","Copyright Hub Board","gardenflat@mac.com ","","28/11/2014 14:00","28/11/2014 18:00"],
+["HDD4","Maxine Horn","Creative Barcode","m.j.horn@creativebarcode.com","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDE1","Evelyn Wilson","CreativeWorks","evelyn@cwlondon.org.uk","020 7420 9443","28/11/2014 09:00","28/11/2014 17:00"],
+["HDE2","James Cobb","Crowd Connected Ltd","james@crowdconnected.co.uk","01483 688 305 / 07798 842532","28/11/2014 09:00","28/11/2014 17:00"],
+["HDE3","Dariush Golzarmanesh","CTO","dariushg@nla.co.uk","","28/11/2014 09:00","28/11/2014 17:00"],
+["HDE4","Edward Averdieck","Cue Songs","ed@cuesongs.com","07515 100 995","28/11/2014 12:00","28/11/2014 14:00"],
+["HDF1","Sejul Malde","Culture24","Sejul@culture24.org.uk","01273 623 266","28/11/2014 09:00","28/11/2014 17:00"],
+["HDF2","Steve Olesansky","DeepSine Ltd (Petri)","deepsine@gmail.com","07787 407 222","28/11/2014 09:00","28/11/2014 17:00"],
+["HDF3","David Hoffman","EPUK","david@hoffmanphotos.com","","28/11/2014 09:00","28/11/2014 17:00"]
+]
+
+
+var empdesks = [
+["ED01","Alex Vail","Events Manager","alex.vail@cde.catapult.org.uk",""],
+["ED02","Allyssa Gregory","HR Manager","allyssa.gregory@cde.catapult.org.uk",""],
+["ED03","Avril Grieves","","avril.grieves@cde.catapult.org.uk",""],
+["ED04","Azita Esmaili","Chief Development Officer","azita@cde.catapult.org.uk",""],
+["ED05","Carly Glowotz","HR Specialist","carly.glowotz@cde.catapult.org.uk","07964572424"],
+["ED06","Casey Richardson","","casey.richardson@cde.catapult.org.uk",""],
+["ED07","Chirdeep Chhabra","","chirdeep.chhabra@cde.catapult.org.uk","+44 7972770116"],
+["ED08","Chris Thompson","Partnerships Director","chris.thompson@cde.catapult.org.uk","07904832928"],
+["ED09","Claire Mackay","Office Manager","claire.mackay@cde.catapult.org.uk","0775 1191 807"],
+["ED10","Clare Brooke","","clare.brooke@cde.catapult.org.uk",""],
+["ED11","Daniel Casadei","","Dan.Casadei@cde.catapult.org.uk",""],
+["ED12","Daniel Dearing","Partnerships Manager","daniel.dearing@cde.catapult.org.uk","07970227860"],
+["ED13","David Sparks","Operations Manager","david.sparks@cde.catapult.org.uk",""],
+["ED14","Dennis Temper","","dennis.temper@cde.catapult.org.uk",""],
+["ED15","Finance Finance","","finance@cde.catapult.org.uk",""],
+["ED16","Gerrod Taylor","","gerrod.taylor@cde.catapult.org.uk",""],
+["ED17","hamid falaki","Connected Digital Economy Catapult","hamid.falaki@cde.catapult.org.uk",""],
+["ED18","Irene Lopez de Vallejo","","Irene.lopezdevallejo@cde.catapult.org.uk",""],
+["ED19","jenni young","","jenni.young@cde.catapult.org.uk",""],
+["ED20","Lee Tarrant","Head of Test and Engineering","","lee.tarrant@cde.catapult.org.uk","0203 574 4894"],
+["ED21","Maame-Yaa Bempah","Financial Controller","mbempah@cde.catapult.org.uk","07964 140 565"],
+["ED22","Marko Balabanovic","Innovation Director","marko.balabanovic@cde.catapult.org.uk",""],
+["ED23","Matt Leipnik","Information Systems Manager","matt.leipnik@cde.catapult.org.uk","07949935777"],
+["ED24","Matt Ward","","matt.ward@cde.catapult.org.uk",""],
+["ED25","Maurizio Pilu","","maurizio.pilu@cde.catpult.org.uk",""],
+["ED26","Michella Oswald","Events Manager","michella.oswald@cde.catapult.org.uk","077712545115"],
+["ED27","Miles Davenport","","miles.davenport@cde.catapult.org.uk",""],
+["ED28","Neil Crockett","","neil.crockett@cde.catapult.org.uk",""],
+["ED29","Nithin Raj","Programme Manager","nithin.raj@cde.catapult.org.uk",""],
+["ED30","Noeline Sanders","Chief Business Officer","noeline.sanders@cde.catapult.org.uk","07786 916826"],
+["ED31","Pat McAlinden","EA to CEO","pat.mcalinden@cde.catapult.org.uk","07964324029"],
+["ED32","Paul Galwas","","paul.galwas@cde.catapult.org.uk","442035744832"],
+["ED33","Richard French","","richard.french@cde.catapult.org.uk",""],
+["ED34","Sade Laja","Head of digital engagement and content","sade.laja@cde.catapult.org.uk","+44 (0) 203 574 4707"],
+["ED35","Steven Wood","Senior Partnerships Manager","steven.wood@cde.catapult.org.uk","07972871092"],
+["ED36","Temitope Adejumo","Accounts Assistant","temitope.adejumo@cde.catapult.org.uk","07946889261"]
+]
+
+
+var eventspaces = [
+["EVSP01","Fostering IoT entreeurship workshop","28/11/2014 09:00","28/11/2014 14:00","http://www.digitalcatapultcentre.org.uk/event/fostering-iot-entrepreneurship/",""],
+["EVSP02","Digital TV Group Global TV Innovation Detonator","01/12/2014 09:00","01/12/2014 12:30","http://www.digitalcatapultcentre.org.uk/event/dtg-global-tv-innovation-detonator/",""],
+["EVSP03","Catapult Programme SME Event","02/12/2014 17:00","02/12/2014 21:30","http://www.digitalcatapultcentre.org.uk/event/cross-catapult-sme-event/",""],
+["EVSP04","101 Digital Breakfast: A new blueprint for Copyright Licensing","03/12/2014 09:00","03/12/2014 10:30","http://www.digitalcatapultcentre.org.uk/event/101-digital-breakfast-future-of-copyright-licensing/","Dr. Dinusha Mendis, Bournemouth University|Dr. Davide Secchi, Bournemouth University"],
+["EVSP05","Copyright Sandpit: Interactive SME  workshop","03/12/2014 11:00","03/12/2014 14:00","http://www.digitalcatapultcentre.org.uk/event/understanding-uk-copyright-law-an-interactive-workshop-for-photographers-and-illustrators/ ","Prof. Ronan Deazley, Professor of Copyright Law - University of Glasgow. Editor, Copyright User Portal|Bartolomeo Meletti, Lead Producer - Copyright User Portal|Dr. Dinusha Mendis, Co Director of CIPPM - Bournemouth University. Lead Academic, Copyright User Portal|Dominic Young, CEO - The Copyright Hub"],
+["EVSP06","IBM Hackathon","03/12/2014 10:00","04/12/2014 00:00","http://www.digitalcatapultcentre.org.uk/event/digital-health-hack-day/ ",""],
+["EVSP07","Content, Customers & Communities in the Media Landscape","04/12/2014 09:30","04/12/2014 16:30","http://www.digitalcatapultcentre.org.uk/event/content-customers-and-communities/ ","Ruben Kostucki, Growth Director - Makers Academy|James Woudhuysen, Futurologist|Lisa Mandell, Co-Founder - QuillContent|Kerensa Jennings, Head of Strategic Delivery - BBC"],
+["EVSP10","CREATe Public Domain Workshop for Creative Businesses","05/12/2014 09:00","05/12/2014 14:00","http://www.digitalcatapultcentre.org.uk/event/create-public-domain-workshop-for-creative-businesses-2/ ",""],
+["EVSP07","Gaming Intelligence Through Data","08/12/2014 09:00","08/12/2014 16:00","http://www.digitalcatapultcentre.org.uk/event/gaming-intelligence-through-data/ ","Nicholas Lovell, Director - GAMESbrief"],
+["EVSP10","D5 Summit","10/12/2014 09:00","10/12/2014 14:00","",""],
+["EVSP07","Digital Health Pitstop","08/12/2014 09:00","12/12/2014 17:30","http://www.digitalcatapultcentre.org.uk/event/healthpitstop/ ",""],
+["EVSP02","Catch Me If You Can: Business Opportunities in Cyber Security","15/12/2014 09:00","15/12/2014 13:00","http://www.digitalcatapultcentre.org.uk/event/catch-me-if-you-can-business-opportunities-in-cyber-security/ ","Sam Keayes, VP Consulting & Cyber Security Services (CIC) - Quintec a Thales Business|Rob Price, Head of Digital UK&I - Atos Consulting|Mark Brown, Executive Director Cyber Security & Resilience - EY"],
+["EVSP10","Data is the New Money: Opportunities in Big Data, Now and in the Future","15/12/2014 13:00","15/12/2014 17:00","http://www.digitalcatapultcentre.org.uk/event/data-is-the-new-money-opportunities-in-big-data-now-and-in-the-future/ ","Carlo Gagliardi, Partner & Co-Lead - Digital Practice|Shahbaz Haque, Director - Hitachi Consulting|John Levell, Associate Partner and UK&I Head of Digital - EY"],
+["EVSP03","Reinventing the High Street","16/12/2014 09:30","16/12/2014 17:30","http://www.digitalcatapultcentre.org.uk/event/reinventing-the-high-street-conference/ ","Ben Brown, Founder - Shopwave|Jonathan Duffy, Managing Director - beacontent|Michelle Lowe FAcSS, University of Southampton"],
+["EVSP05","UK Digital Summit for Policy & Business","17/12/2014 09:00","17/12/2014 17:00","http://www.digitalcatapultcentre.org.uk/event/uk-digital-summit/ ","Mary Kyriakidi, VP Insight for Branded Services and Content – BBC Worldwide|Paul Spence, Director of Strategy and Corporate Affairs – EDF Energy|Paul Gainham, Senior Director of Vertical Marketing EMEA – Juniper Networks|"],
+["EVSP01","IBM Hackathon","17/12/2014 10:00","17/12/2014 00:00","",""],
+["EVSP09","When Data Becomes Knowledge: Catapult Staff Conference","18/12/2014 11:00","18/12/2014 16:00","",""],
+["EVSP02","101 Launch Season Closing Party","18/12/2014 18:00","18/12/2014 22:00","",""]
+]
+
+String.format = function() {
+    // The string containing the format items (e.g. "{0}")
+    // will and always has to be the first argument.
+    var theString = arguments[0];
+    // start with the second argument (i = 1)
+    for (var i = 1; i < arguments.length; i++) {
+        // "gm" = RegEx options for Global search (more than one instance)
+        // and for Multiline search
+        var regEx = new RegExp("\\{" + (i - 1) + "\\}", "gm");
+        theString = theString.replace(regEx, arguments[i]);
+    }
+    return theString;
+}
+
+
+String.capitalize = function() {
+    return this.toLowerCase().charAt(0).toUpperCase() + this.slice(1);
+}
+
+
+var getElementData = function(elements, elementId) {
+    var result = ["N/A","N/A","N/A","N/A","N/A","N/A","N/A"];
+    for (e in elements) {
+        if (elements[e][0] == elementId) {
+            result = elements[e];
+        };
+    };
+    return result;
+}
+
+
+var getDetails = function(elements, elementId) {
+   var element = getElementData(eval(elements), elementId); 
+
+   var HDcols = ["Desk","Name","Organistation","Email","Phone","Time in","Time Out"];
+   var EDcols = ["Desk","Name","Role","Email","Phone"];
+   var EVcols = ["Event Space","Name","Start","Finish","Link","Speakers"]
+
+   var output = "<table id='deskDetails' class='deskDetails'>";
+   var cellTemplate = "<tr id='{0}Cell' class='tooltipCell'>"+
+                           "<td colspan=2 id='{1}Field' class='tooltipField'>{2}</td>"+
+                           "<td id='{3}Value' class='tooltipValue'>{4}</td>"+
+                       "</tr>";
+
+   var cols = null;
+   switch (elements) {
+       case "empdesks":
+           cols = EDcols;
+           console.log("EDCols");
+           break;
+       case "eventspaces":
+           cols = EVcols;
+           console.log("EVCols");
+           break;
+       case "hotdesks":
+           cols = HDcols;
+           console.log("HDCols");
+           break;
+   }
+
+   for (col in cols) {
+        c = String.format(cellTemplate,
+                          cols[col].replace(" ",""), 
+                          cols[col].replace(" ",""),
+                          cols[col],
+                          cols[col].replace(" ",""),
+                          element[col])
+        output += c;
+   }
+   output += "</table>";
+   return output;
+}
+
+
+!function ($) {
+  $(function(){
+
+    $("div[id*='EVSP']").click(function() {
+        $(this).tooltip({
+            items: "[id]",
+            content: function() {
+                var element = $( this );
+                return getDetails("eventspaces", element.context.id);
+            },
+            //show: "slideDown", // show immediately
+            show: { effect: function() { $(this).fadeTo(300, 0.9);}, length:0},
+        });
+        $(this).tooltip("open");
+    });
+
+    $("div[id*='HD']").click(function() {
+        $(this).tooltip({
+            items: "[id]",
+            content: function() {
+                var element = $( this );
+                return getDetails("hotdesks", element.context.id);
+            },
+            //show: "slideDown", // show immediately
+            show: { effect: function() { $(this).fadeTo(300, 0.9);}, length:0},
+        });
+        $(this).tooltip("open");
+    });
+
+    $("div[id*='ED']").click(function() {
+        $(this).tooltip({
+            items: "[id]",
+            content: function() {
+                var element = $( this );
+                return getDetails("empdesks", element.context.id);
+            },
+            //show: "slideDown", // show immediately
+            show: { effect: function() { $(this).fadeTo(300, 0.9);}, length:0},
+        });
+        $(this).tooltip("open");
+    });
+  });
+}(window.jQuery);
